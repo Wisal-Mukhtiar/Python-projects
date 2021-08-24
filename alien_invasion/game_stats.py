0689aaf5c2a6f8914b645a5ee67090936c42eab7
Wisal Mukhtiar <63037195+Wisal-Mukhtiar@users.noreply.github.com>
2021-08-24 18:53:24 +0500

    stats updated accordingly to scoreboards and buttons

--- alien_invasion/game_stats.py
+++ alien_invasion/game_stats.py
@@ -1,13 +1,19 @@
 class GameStats:
     """Track statistics for alien Invasion"""
+
     def __init__(self, ai_game):
         """init the stats"""
         self.settings = ai_game.settings
         self.reset_stats()
 
-        #start the game in an active state
-        self.game_active = True
+        # start the game in an active state
+        self.game_active = False
 
+        # scores
+        self.score = 0
+
+        # high score should never be reset
+        self.high_score = 0
 
     def reset_stats(self):
         """init the statistics that can change during the game """
