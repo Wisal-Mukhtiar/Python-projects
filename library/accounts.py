379b61a0d65a503553f6a2763863a9bd8bf1c9d6
Wisal Mukhtiar <63037195+Wisal-Mukhtiar@users.noreply.github.com>
2021-08-29 11:01:36 +0500

    addition to accounts and removal from books code

--- library/accounts.py
+++ library/accounts.py
@@ -2,6 +2,9 @@
 
 from enum import Enum
 
+from books import Book
+
+
 class IdStatus(Enum):
     none, active, blocked, blacklisted, cancelled = 0, 1, 2, 3, 4,
 
@@ -24,6 +27,23 @@ class Librarian(Account):
     def __int__(self, name, email, phone, admin_id, id_status=IdStatus.active):
         super().__init__(name, email, phone, id_status)
         self.id = admin_id
+        self.book = Book()
+        self.member = MemberAccount()
+
+    def add_book(self):
+        self.book.name = input("enter book name : ")
+        self.book.authors = input("Enter author's names with a comma and space : ").split(', ')
+        self.book.publisher = input("Enter book publishers : ")
+
+    def Remove_book(self):
+        # well enter the accession or isbn number of the book to remove that book
+        pass
+
+    def add_member_account(self):
+
+
+
+
 
 
 class MemberAccount(Account):
--- library/books.py
+++ library/books.py
@@ -1,20 +1,12 @@
 """module contain info about books of library"""
 
 
-class Books:
+class Book:
     """Books class containing book information and stats"""
 
-    def __init__(self, name, author_name, publisher, accession_number):
-        self.name = name
-        self.author_name = author_name
-        self.publisher = publisher
-        self.accession_number = accession_number
+    def __init__(self):
+        self.name = None
+        self.publisher = None
+        self.authors = None
 
-    def add_book(self):
-        self.name = input("Enter book name : ")
-        self.author_name = input("Enter author's name:  ")
-        self.publisher = input("Enter publisher's name")
 
-    def remove_book(self):
-        remove_book_isbn = input("Enter isbn of the book: ")
-        remove_book_accNum = input("Enter the acession number of the book to be removed : ")
